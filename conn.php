8ae1f834a91968b1d3719d33abc3024db0aebaef
kimesplago03-cyber <kimesplago03@gmail.com>
2026-06-05 02:08:48 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $server     = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "dbspirited1";
+$dbname     = "dbspirited";
 $tablelog   = "register";
 $tablelogin = "adminlogin";
 
